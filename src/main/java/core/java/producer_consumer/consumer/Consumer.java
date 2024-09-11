4408cfd65c60cdefd813d3cef5c628c2308f7772
sakshijain20 <jain.sakshi1099@gmail.com>
2024-09-11 12:58:50 +0530

    created consumer

---
+++ src/main/java/core/java/producer_consumer/consumer/Consumer.java
@@ -0,0 +1,49 @@
+package core.java.producer_consumer.consumer;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import core.java.producer_consumer.messageQueue.Message;
+import core.java.producer_consumer.messageQueue.MessageQueue;
+
+public class Consumer implements Runnable{
+
+	private final MessageQueue messageQueue;
+    private final AtomicInteger success, failure;
+
+    public Consumer(MessageQueue messageQueue, AtomicInteger success, AtomicInteger failure) {
+        this.messageQueue = messageQueue;
+        this.success = success;
+        this.failure = failure;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+        	
+            try {
+                Message data = messageQueue.consume();
+                
+                if (data.getData().equals("stop")) {
+                    break;
+                }
+                
+                processMessage(data);
+                success.incrementAndGet();
+            } 
+            catch (Exception e) {
+                failure.incrementAndGet();
+            }
+        }
+    }
+
+    private void processMessage(Message data) {
+        boolean isInValid = Boolean.parseBoolean(data.getData());
+
+        if (isInValid) {
+            throw new RuntimeException("Failed to process the message: " + data);
+        }
+
+        System.out.println("The message is processed successfully: " + data);
+    }
+   
+}
