060edf5d3eab3255922db5160e44bcf0343978be
sakshijain20 <jain.sakshi1099@gmail.com>
2024-09-11 12:30:53 +0530

    init commit

---
+++ src/main/java/core/java/producer_consumer/App.java
@@ -0,0 +1,10 @@
+package core.java.producer_consumer;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/core/java/producer_consumer/AppTest.java
@@ -0,0 +1,19 @@
+package core.java.producer_consumer;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
