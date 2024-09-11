3e6db6969e161c3905e155cd2970ccaf8e3670f9
sakshijain20 <jain.sakshi1099@gmail.com>
2024-09-11 14:04:33 +0530

    implemented test class for MessageQueue

---
+++ src/test/java/core/java/producer_consumer/MessageQueueTest.java
@@ -0,0 +1,66 @@
+package core.java.producer_consumer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import core.java.producer_consumer.messageQueue.Message;
+import core.java.producer_consumer.messageQueue.MessageQueue;
+
+public class MessageQueueTest {
+
+	MessageQueue messageQueue;
+	
+	@Test
+    void emptyQueueTest() throws Exception {
+        messageQueue = new MessageQueue(2);
+
+        Thread consumerThread = new Thread(() -> {
+            try {
+                messageQueue.consume();
+            } catch (Exception e) {
+                fail("Consumer should wait instead of throwing an exception");
+            }
+        });
+        consumerThread.start();
+        Thread.sleep(1000);
+        assertTrue(consumerThread.isAlive());
+
+        messageQueue.produce(new Message("false"));
+        consumerThread.join();
+    }
+	
+    @Test
+    void fullQueueTest() throws Exception {
+    	messageQueue = new MessageQueue(2);
+        messageQueue.produce(new Message("true"));
+        messageQueue.produce(new Message("true"));
+
+        Thread producerThread = new Thread(() -> {
+            try {
+                messageQueue.produce(new Message("false"));
+            } catch (Exception e) {
+                fail("Producer should wait instead of throwing an exception");
+            }
+        });
+        producerThread.start();
+        Thread.sleep(1000);
+        assertTrue(producerThread.isAlive());
+
+        messageQueue.consume();
+        producerThread.join();
+    }
+
+    @Test
+    void produceAndConsumeTest() throws Exception {
+        messageQueue = new MessageQueue(3);
+        Message message = new Message("true");
+        messageQueue.produce(message);
+
+        Message consumedMessage = messageQueue.consume();
+        assertEquals(message, consumedMessage);
+    }
+
+}
