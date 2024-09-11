215aad8c503058c944dcf6bed7088777a5d50d24
sakshijain20 <jain.sakshi1099@gmail.com>
2024-09-11 13:09:02 +0530

    implemeneted main class

--- src/main/java/core/java/producer_consumer/App.java
+++ src/main/java/core/java/producer_consumer/App.java
@@ -1,10 +1,45 @@
 package core.java.producer_consumer;
 
-/**
- * Hello world!
- */
+import java.util.concurrent.atomic.AtomicInteger;
+
+import core.java.producer_consumer.messageQueue.Message;
+import core.java.producer_consumer.messageQueue.MessageQueue;
+import core.java.producer_consumer.producer.Producer;
+import core.java.producer_consumer.consumer.Consumer;
+
+import java.util.List;
+
 public class App {
+	
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	MessageQueue messageQueue = new MessageQueue(5);
+        AtomicInteger success = new AtomicInteger();
+        AtomicInteger failures = new AtomicInteger();
+
+        Thread producerThread = new Thread(new Producer(messageQueue,
+        												List.of(new Message("true"),
+        														new Message("false"),
+        														new Message("true"),
+        														new Message("true"),
+        														new Message("false"),
+        														new Message("true"),
+        														new Message("false"),
+        														new Message("stop")
+        												)));
+
+        Thread consumerThread = new Thread(new Consumer(messageQueue, success, failures));
+
+        producerThread.start();
+        consumerThread.start();
+
+        try {
+            producerThread.join();
+            consumerThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Total messages processed successfully: " + success.get());
+        System.out.println("Total failures encountered: " + failures.get());
     }
 }
